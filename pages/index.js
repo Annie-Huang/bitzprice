3cadc239324d3a5a632b512327eb0379862289be
Annie-Huang <annie.hn@gmail.com>
2019-07-14 11:25:31 +1000

    Create basic structure for Prices component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
-import Layout from "../components/Layout";
+import fetch from 'isomorphic-unfetch';
 
-const Index = () => (
+import Layout from "../components/Layout";
+import Prices from "../components/Prices";
+
+const Index = (props) => (
     <Layout>
         <div>
             <h1>Welcome to BitzPrice</h1>
+            {props.time.updated}
+            <Prices></Prices>
         </div>
     </Layout>
 );
+
+Index.getInitialProps = async () => {
+    const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+    const data = await res.json();
+
+    return {
+        time: data.time,
+        bpi: data.bpi
+    }
+};
 
 export default Index;
