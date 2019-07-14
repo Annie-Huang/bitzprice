aea8611eb0f39dad7a569728dd03f929d5154f4a
Annie-Huang <annie.hn@gmail.com>
2019-07-14 10:59:19 +1000

    Import bootswatch's cerulean theme and comment out  style component (it's just for show you how to do it before)

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,24 +7,24 @@ const Navbar = () => (
             <li><Link href="/about"><a>About</a></Link></li>
         </ul>
 
-        <style jsx>{`
-            ul {
-                background: #333;
-                color: #fff;
-                list-style: none;
-                display: flex;
-            }
-            
-            ul li {
-                font-size: 18px;
-                margin-right: 20px;
-            }
-            
-            ul li a {
-                color: #fff;
-                text-decoration: none;
-            }
-        `}</style>
+        {/*<style jsx>{`*/}
+        {/*    ul {*/}
+        {/*        background: #333;*/}
+        {/*        color: #fff;*/}
+        {/*        list-style: none;*/}
+        {/*        display: flex;*/}
+        {/*    }*/}
+        {/*    */}
+        {/*    ul li {*/}
+        {/*        font-size: 18px;*/}
+        {/*        margin-right: 20px;*/}
+        {/*    }*/}
+        {/*    */}
+        {/*    ul li a {*/}
+        {/*        color: #fff;*/}
+        {/*        text-decoration: none;*/}
+        {/*    }*/}
+        {/*`}</style>*/}
     </div>
 );
 
